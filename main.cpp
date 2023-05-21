2f94f6ec2b3ad192730dc7580696043b228e943a
mixiki <132454987+mixiki@users.noreply.github.com>
2023-05-21 21:05:23 +0700

    .json parser

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "game.hpp"
+#include <nlohmann/json.hpp>
 using namespace std;
+using json = nlohmann::json;
 
+// for convenience
 int main(){
     const int screenWidth = 1280;
     const int screenHeight = 720;
@@ -10,5 +13,12 @@ int main(){
     while (!game.GameShouldClose()){
         game.Tick();
     }
+
+    /*.json file located in build folder*/
+    std::ifstream i("test.json");
+    json j;
+    i >> j;
+    std::cout << j;
+    
     return 0;
 }
