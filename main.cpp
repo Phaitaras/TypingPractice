7798cc0736949b38a4bacbbbd11315d9ccbffc01
mixiki <132454987+mixiki@users.noreply.github.com>
2023-05-23 15:21:41 +0700

    Revert ".json file can be outside build now"
    This reverts commit 98175f6dc12db4e5cbbeb2876e987848e636fc22.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 // for convenience
 int main(){
-        const int screenWidth = 1280;
-        const int screenHeight = 720;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-        Game game{screenWidth, screenHeight, "Typing Game"};
+    Game game{screenWidth, screenHeight, "Typing Game"};
 
-        while (!game.GameShouldClose()){
-            game.Tick();
-        }
+    while (!game.GameShouldClose()){
+        game.Tick();
+    }
 
-        return 0;
+    return 0;
 }
